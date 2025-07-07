263b5a2cd8ed9469d0e21bc562e62aea0be2ae69
Patrick.Lanham <p.lanham91@icloud.com>
2025-07-07 13:58:12 +0200

    Initial Commit

---
+++ Fokus/FokusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FokusApp.swift
+//  Fokus
+//
+//  Created by Patrick Lanham on 07.07.25.
+//
+
+import SwiftUI
+
+@main
+struct FokusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
